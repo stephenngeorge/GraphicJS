0412961fdfd5b105a654bb816a4c6cd353f9dd0e
Stephen George <stephenngeorge@gmail.com>
2017-03-11 19:26:52 +0000

    basic restriction of x and y

--- entry.js
+++ entry.js
@@ -6,16 +6,23 @@ const { c, width, height, id } = g.canvas({
   el: document.getElementById('canvas-container')
 });
 
-
-let ball = g.circle(c, width * .5, height * .5);
+let ball = g.circle(c, width * .5, height * .5, 20);
+let circ = g.circle(c, width * .3, 0 + 50, 20);
 
 let bodies = {};
 bodies.ball = new Body(ball);
+bodies.circ = new Body(circ);
 
-const engine = new Engine(bodies);
+let engine = new Engine(bodies, {
+  context: c,
+  boundX: true,
+  boundY: true
+});
 
 g.animate(() => {
+  engine.step();
+
   g.bgSolid(c);
-  ball.draw();
-  engine.update();
-}, 24, 100);
+  ball.draw().outline();
+  circ.draw().outline();
+}, 96);
--- graphic-engine/Engine.js
+++ graphic-engine/Engine.js
@@ -1,9 +1,12 @@
 export default class Engine {
 
-  constructor(bodies, { gravity_x = 0, gravity_y = 1 } = {}) {
+  constructor(bodies, { gravity_x = 0, gravity_y = 1, context = null, boundX = false, boundY = false } = {}) {
     this.bodies = bodies;
     this.gravity_x = gravity_x;
     this.gravity_y = gravity_y;
+    this.context = context;
+    this.boundX = boundX;
+    this.boundY = boundY;
   }
 
   applyForces(body) {
@@ -13,8 +16,19 @@ export default class Engine {
     return body;
   }
 
-  update() {
+  restrictX(body) {
+    if (body.entity.pos.x >= this.context.canvas.clientWidth) body.entity.pos.x = this.context.canvas.clientWidth;
+    if (body.entity.pos.x <= 0) body.entity.pos.x = 0;
+  }
+  restrictY(body) {
+    if (body.entity.pos.y >= this.context.canvas.clientHeight) body.entity.pos.y = this.context.canvas.clientHeight;
+    if (body.entity.pos.y <= 0) body.entity.pos.y = 0;
+  }
+
+  step() {
     for (let prop in this.bodies) {
+      if (!!this.boundX) this.restrictX(this.bodies[prop]);
+      if (!!this.boundY) this.restrictY(this.bodies[prop]);
       this.applyForces(this.bodies[prop]);
     }
     return this;
